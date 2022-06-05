0e8135e482f8f33912846c1a0c9f33358bfd31b2
Manuel Céspedes <virgiliomca@gmail.com>
2022-06-05 10:27:29 -0400

    Soporte para tiempo del mensaje y tipo de mensaje para la barra.

--- LogWriter/LogWriter.vb
+++ LogWriter/LogWriter.vb
@@ -1,7 +1,7 @@
 Imports System.Data.SqlClient
 Imports SAPbouiCOM
 Public Class LogWriter
-
+    'BEGIN: grupo #1
     Public Shared Sub WriteLog(ByVal pMensaje As String)
         Try
             Microsoft.VisualBasic.FileIO.FileSystem.WriteAllText(
@@ -22,6 +22,26 @@ Public Class LogWriter
         End Try
     End Sub
 
+    Public Shared Sub WriteLog(ByVal pMensaje As String, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime)
+        Try
+            WriteLog(pMensaje)
+            pStatusBar.SetText(pMensaje, pMessageTime, BoStatusBarMessageType.smt_Warning)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Shared Sub WriteLog(ByVal pMensaje As String, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime, ByVal pStatusMessageType As BoStatusBarMessageType)
+        Try
+            WriteLog(pMensaje)
+            pStatusBar.SetText(pMensaje, pMessageTime, pStatusMessageType)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    'END:
+
+    'BEGIN: grupo #2
     Public Shared Sub WriteLog(ByVal e As Excepcion)
         Try
             WriteLog(e.Message())
@@ -39,6 +59,26 @@ Public Class LogWriter
         End Try
     End Sub
 
+    Public Shared Sub WriteLog(ByVal e As Excepcion, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime)
+        Try
+            WriteLog(e.Message())
+            pStatusBar.SetText(e.Message(), pMessageTime, BoStatusBarMessageType.smt_Warning)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Shared Sub WriteLog(ByVal e As Excepcion, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime, ByVal pStatusMessageType As BoStatusBarMessageType)
+        Try
+            WriteLog(e.Message())
+            pStatusBar.SetText(e.Message(), pMessageTime, pStatusMessageType)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    'END:
+
+    'BEGIN: grupo #3
     Public Shared Sub WriteLog(ByVal e As Exception)
         Try
             WriteLog(e.StackTrace())
@@ -56,6 +96,26 @@ Public Class LogWriter
         End Try
     End Sub
 
+    Public Shared Sub WriteLog(ByVal e As Exception, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime)
+        Try
+            WriteLog(e.StackTrace())
+            pStatusBar.SetText(e.StackTrace(), pMessageTime, BoStatusBarMessageType.smt_Warning)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Shared Sub WriteLog(ByVal e As Exception, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime, ByVal pStatusMessageType As BoStatusBarMessageType)
+        Try
+            WriteLog(e.StackTrace())
+            pStatusBar.SetText(e.StackTrace(), pMessageTime, pStatusMessageType)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    'END:
+
+    'BEGIN: grupo #4
     Public Shared Sub WriteLog(ByVal e As SqlException)
         Try
             WriteLog(e.StackTrace())
@@ -72,4 +132,23 @@ Public Class LogWriter
 
         End Try
     End Sub
+
+    Public Shared Sub WriteLog(ByVal e As SqlException, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime)
+        Try
+            WriteLog(e.StackTrace())
+            pStatusBar.SetText(e.StackTrace(), pMessageTime, BoStatusBarMessageType.smt_Warning)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Shared Sub WriteLog(ByVal e As SqlException, ByRef pStatusBar As SAPbouiCOM.StatusBar, ByVal pMessageTime As BoMessageTime, ByVal pStatusMessageType As BoStatusBarMessageType)
+        Try
+            WriteLog(e.StackTrace())
+            pStatusBar.SetText(e.StackTrace(), pMessageTime, pStatusMessageType)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    'END:
 End Class
